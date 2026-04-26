803548eb5cc60de81f44e15a1f8155b558b5556c
shaikhahan03 <shaikhahan03@gmail.com>
2026-04-27 01:04:51 +0530

    Add transaction statement display functionality

--- statement.py
+++ statement.py
@@ -1,1 +1,9 @@
+import utils
 
+def show_statement():
+    print("\n--- Transaction Statement ---")
+    if len(utils.transactions) == 0:
+        print("No transactions yet.")
+    else:
+        for transaction in utils.transactions:
+            print(transaction)
