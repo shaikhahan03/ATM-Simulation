564666772a8ebb28c0fc17c7f00ff60d4e3f461b
shaikhahan03 <shaikhahan03@gmail.com>
2026-04-27 01:06:19 +0530

    Add deposit_money function to handle deposits

--- deposit.py
+++ deposit.py
@@ -1,1 +1,11 @@
+import utils
 
+def deposit_money():
+    amount = float(input("Enter amount to deposit: ₹"))
+
+    if amount > 0:
+        utils.balance += amount
+        utils.transactions.append(f"Deposited ₹{amount}")
+        print("Amount deposited successfully!")
+    else:
+        print("Invalid amount!")
