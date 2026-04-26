c4ba8f0a84e262173b49262d1a8a80356829d969
shaikhahan03 <shaikhahan03@gmail.com>
2026-04-27 01:05:58 +0530

    Implement withdraw_money function for transactions

--- withdraw.py
+++ withdraw.py
@@ -1,1 +1,13 @@
+import utils
 
+def withdraw_money():
+    amount = float(input("Enter amount to withdraw: ₹"))
+
+    if amount <= 0:
+        print("Invalid amount!")
+    elif amount > utils.balance:
+        print("Insufficient balance!")
+    else:
+        utils.balance -= amount
+        utils.transactions.append(f"Withdrawn ₹{amount}")
+        print("Amount withdrawn successfully!")
